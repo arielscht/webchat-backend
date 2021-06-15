b807fbc3d9a4085874a74abe4ddea51ea62f80bc
Ariel Evaldt Schmitt <ariel-evaldt@hotmail.com>
2021-06-15 11:53:00 -0300

    fix pg ssl error

--- knexfile.js
+++ knexfile.js
@@ -36,7 +36,12 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    },
     pool: {
       min: 2,
       max: 10
@@ -44,7 +49,7 @@ module.exports = {
     migrations: {
       directory: './src/database/migrations',
       tableName: 'knex_migrations'
-    }
+    },
   }
 
 };
